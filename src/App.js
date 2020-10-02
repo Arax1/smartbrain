e4a59e656aaedd6ffc7ce5265d49ac76d1677535
Arax1 <anandraju55@hotmail.com>
2020-10-02 16:19:36 -0400

    added signin functionality to store passwords

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends Component {
     }
   }
 
+
   calculateFaceLocation = (data) => {
     const clarafaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
